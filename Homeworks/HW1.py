9610394ab370fbf6f8bf49eb50417903809b9324
DilaraMavi <50704332+DilaraMavi@users.noreply.github.com>
2021-02-17 18:48:49 +0300

    Update HW1.py

--- Homeworks/HW1.py
+++ Homeworks/HW1.py
@@ -1,5 +1,19 @@
-#Explain your work
+# Hello Python
 
-#Question 1
-for x in range(a):
-	print(a)
+# HMW 1 : Generating a 3x3 matrix with 9 'prime' numbers using the for loop.
+
+import random as rnd 
+import secrets as scr
+
+primeNumberList = []
+
+for num in range(1,100):  
+   if num > 1:  
+       for i in range(2,num):  
+           if (num % i) == 0:  
+               break  
+       else:  
+           primeNumberList.append(num)
+
+for i in range(3):
+  print(scr.choice(primeNumberList), scr.choice(primeNumberList), scr.choice(primeNumberList))
